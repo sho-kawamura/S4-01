7c01eae80e7267f6a19f4d794e5ab6b00a05b817
sho-kawamura <s.kawamura@lincraft.co.jp>
2019-11-20 16:54:24 +0900

    11/20 update

--- js/index.js
+++ js/index.js
@@ -244,7 +244,7 @@ $(function() {
         let touchEndY = Math.floor(downPoint[1]);
         console.log("onMouseUp_touchEndX:" + touchEndX);
         console.log("onMouseUp_touchEndY:" + touchEndY);
-        if (Math.abs(touchStartX - touchEndX) < 10 && Math.abs(touchStartY - touchEndY) < 10) {
+        if (Math.abs(touchStartX - touchEndX) < (cellSize/2)*scale && Math.abs(touchStartY - touchEndY) < (cellSize/2)*scale) {
             // クリック判定（タッチ開始時座標と終了座標が僅差であればクリックとみなす）
             let innerGridTouch = sps.getNearestGridPoint([touchX, touchY], gridInfo);
             if (null !== innerGridTouch) { //グリッド内部をタッチしているか判定
